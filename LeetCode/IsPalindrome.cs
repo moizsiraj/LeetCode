8570c1eed0a670849d3adf1f6e387b40a31c3e18
Moiz Siraj <abdulmoiz68@gmail.com>
2022-06-16 02:32:17 +0500

    refactoring

---
+++ LeetCode/IsPalindrome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class IsPalindrome
+    {
+        public bool IsPalindromeInt(int x)
+        {
+            Stack<int> s = new Stack<int>();
+            Stack<int> s2 = new Stack<int>();
+            int count = 0;
+            while (x / 10 != 0)
+            {
+                count++;
+                s.Push(Math.Abs(x % 10));
+                x /= 10;
+            }
+            s.Push(x);
+            count++;
+            if (s.Count == 1)
+            {
+                if (s.Pop() < 0)
+                    return false;
+                else
+                    return true;
+            }
+            for (int i = 0; i < count / 2; i++)
+            {
+                s2.Push(s.Pop());
+            }
+            if (count % 2 != 0)
+            {
+                s.Pop();
+            }
+            while (s.Count != 0)
+            {
+                if (s.Pop() != s2.Pop())
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/RomanToInt.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class RomanToInt
+    {
+        public int RomanToIntSol(string s)
+        {
+            int ones = 0;
+            int tens = 0;
+            int hundreds = 0;
+            int thousands = 0;
+
+            char currentChar = s.ElementAt<char>(s.Length - 1);
+            char prevChar = s.Length >= 2 ? s.ElementAt<char>(s.Length - 2) : 'O';
+            
+            while (
+                    currentChar != 'X' &&
+                    currentChar != 'L' &&
+                    currentChar != 'C' &&
+                    currentChar != 'D' &&
+                    currentChar != 'M' &&
+                    s.Length != 0
+                  )
+            {
+                if (currentChar == 'I')
+                {
+                    ones++;
+                }
+                else if (currentChar == 'V' && prevChar == 'I')
+                {
+                    ones += 4;
+                    s = s[..^1];
+                }
+                else
+                {
+                    ones += 5;
+                }
+                if (s.Length >= 1)
+                    s = s[..^1];
+                if (s.Length > 0)
+                {
+                    currentChar = s.ElementAt<char>(s.Length - 1);
+                    prevChar = s.Length >= 2 ? s.ElementAt<char>(s.Length - 2) : 'O';
+                }
+            }
+
+            while   ( 
+                    currentChar != 'C' &&
+                    currentChar != 'D' &&
+                    currentChar != 'M' &&
+                    s.Length != 0
+                    )
+            {
+                if (currentChar == 'X' && prevChar == 'I')
+                {
+                    ones += 9;
+                    s = s[..^1];
+                }
+                else if (currentChar == 'X')
+                {
+                    tens += 10;
+                }
+                else if (currentChar == 'L' && prevChar == 'X')
+                {
+                    tens += 40;
+                    s = s[..^1];
+                }
+                else
+                {
+                    tens += 50;
+                }
+                if (s.Length >= 1)
+                    s = s[..^1];
+                if (s.Length > 0)
+                {
+                    currentChar = s.ElementAt<char>(s.Length - 1);
+                    prevChar = s.Length >= 2 ? s.ElementAt<char>(s.Length - 2) : 'O';
+                }
+            }
+
+            while (currentChar != 'M' && s.Length != 0)
+            {
+                if (currentChar == 'C' && prevChar == 'X')
+                {
+                    tens += 90;
+                    s = s[..^1];
+                }
+                else if (currentChar == 'C')
+                {
+                    hundreds += 100;
+                }
+                else if (currentChar == 'D' && prevChar == 'C')
+                {
+                    hundreds += 400;
+                    s = s[..^1];
+                }
+                else
+                {
+                    hundreds += 500;
+                }
+                if (s.Length >= 1)
+                    s = s[..^1];
+                if (s.Length > 0)
+                {
+                    currentChar = s.ElementAt<char>(s.Length - 1);
+                    prevChar = s.Length >= 2 ? s.ElementAt<char>(s.Length - 2) : 'O';
+                }
+            }
+
+            while (s.Length != 0)
+            {
+                if (currentChar == 'M' && prevChar == 'C')
+                {
+                    hundreds += 900;
+                    s = s[..^1];
+                }
+                else
+                {
+                    thousands += 1000;
+                }
+                if (s.Length >= 1)
+                    s = s[..^1];
+                if (s.Length > 0)
+                {
+                    currentChar = s.ElementAt<char>(s.Length - 1);
+                    prevChar = s.Length >= 2 ? s.ElementAt<char>(s.Length - 2) : 'O';
+                }
+            }
+
+            return ones + tens + hundreds + thousands;
+        }
+    }
+}
