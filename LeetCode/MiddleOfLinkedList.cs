2f1ed8546c084233858863cd13cf891f8c016e66
Moiz Siraj <abdulmoiz68@gmail.com>
2022-09-17 22:31:46 +0500

    middle of linked list

---
+++ LeetCode/MiddleOfLinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class MiddleOfLinkedList
+    {
+        /*My Approach
+         * Use two pointers middle and end
+         * start at head and move middle once and end twice
+         * untill end is null or end.next is null
+         */
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        //11 Mins
+        public ListNode MiddleNode(ListNode head)
+        {
+            ListNode middle = head;
+            ListNode end = head;
+
+            while (end != null && end.next != null)
+            {
+                middle = middle.next;
+                end = end.next.next;
+            }
+            return middle;
+        }
+    }
+}
