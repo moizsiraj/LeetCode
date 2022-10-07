43eaff24227034586e99470c12b1c116e245ea6e
Moiz Siraj <abdulmoiz68@gmail.com>
2022-10-08 01:07:59 +0500

    Maximum subarray sum

---
+++ LeetCode/MaxiumSubArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class MaxiumSubArray
+    {
+        public int MaxSubArrayN3(int[] nums)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    int sum = 0;
+                    for (int k = i; k <= j; k++)
+                    {
+                        sum += nums[k];
+                    }
+                    if (max < sum)
+                        max = sum;
+                }
+            }
+            return max;
+        }
+
+        public int MaxSubArrayN2(int[] nums)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                    if (max < sum)
+                        max = sum;
+                }
+            }
+            return max;
+        }
+
+        public int MaxSubArrayN(int[] nums)
+        {
+            int max = nums[0];
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (max < sum)
+                    max = sum;
+                if(sum < 0)
+                    sum = 0;
+            }
+            return max;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,7 +6,7 @@ public class Solution
 {
     static void Main()
     {
-        var sol = new PlusOne( new int[] { 8,9,9,9 }).Solve();
+        var sol = new MaxiumSubArray().MaxSubArrayN2( new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
         Console.WriteLine(sol);
     }
 }
