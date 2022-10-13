400df8ed481ee97e92d539013f61c32357aee149
Moiz Siraj <abdulmoiz68@gmail.com>
2022-10-13 21:51:28 +0500

    longest substring non repeat o(n) solution

---
+++ LeetCode/LongestNonRepeatingSubstring.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class LongestNonRepeatingSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new Dictionary<char, int>();
+            var left = 0;
+            var right = 0;
+            var len = 0;
+            var n = s.Length;
+
+            while (right < n)
+            {
+                if (set.TryGetValue(s.ElementAt<char>(right), out int valIndex))
+                {
+                    left = Math.Max(valIndex + 1, left);
+                    set[s.ElementAt<char>(right)] = right;
+                }
+                else
+                {
+                    set.Add(s.ElementAt<char>(right), right);
+                }
+                len = Math.Max(len, right - left + 1);
+                right++;
+            }
+            return len;
+        }
+
+        public int LengthOfLongestSubstring2N(string s)
+        {
+            var set = new HashSet<char>();
+            var left = 0;
+            var right = 0;
+            var max = 0;
+            var temp = 0;
+            var length = s.Length;
+
+            while (left < length && right < length)
+            {
+                if (set.Contains(s.ElementAt<char>(right)))
+                {
+                    set.Remove(s.ElementAt(left));
+                    left++;
+                    temp--;
+                }
+                else
+                {
+                    set.Add(s.ElementAt<char>(right));
+                    temp++;
+                    right++;
+
+                    if (temp > max)
+                        max = temp;
+                }
+            }
+            return max;
+        }
+
+        public int LengthOfLongestSubstringNaive(string s)
+        {
+            var set = new HashSet<char>();
+            var max = int.MinValue;
+            for (int i = 0; i < s.Length; i++)
+            {
+                set.Clear();
+                var temp = 0;
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (set.Contains(s.ElementAt<char>(j)))
+                        break;
+                    set.Add(s.ElementAt<char>(j));
+                    temp++;
+                    if (temp > max)
+                        max = temp;
+                }
+            }
+            return max;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,7 +6,7 @@ public class Solution
 {
     static void Main()
     {
-        var sol = new MaxiumSubArray().MaxSubArrayN2( new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+        var sol = new LongestNonRepeatingSubstring().LengthOfLongestSubstring("abcabcbb");
         Console.WriteLine(sol);
     }
 }
