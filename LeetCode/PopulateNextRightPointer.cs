de8bfd199c27989ff8fe4af056e23342042d186b
Moiz Siraj <abdulmoiz68@gmail.com>
2022-10-29 19:28:44 +0300

    Populate Next Node

---
+++ LeetCode/PopulateNextRightPointer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace LeetCode
+{
+    internal class PopulateNextRightPointer
+    {
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+        
+        /* Approach
+         * use BFS to get the levels of the tree
+         * set next for each node in each level
+         */
+
+        //1 hour+ 
+        public Node Connect(Node root)
+        {
+            if (root is null) return root;
+
+            var queue = new Queue<Node>();
+            var list = new List<List<Node>>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                var level = queue.Count;
+                var levelList = new List<Node>();
+                for (int i = 0; i < level; i++)
+                {
+                    var node = queue.Dequeue();
+                    levelList.Add(node);
+                    if (node.left is not null)
+                        queue.Enqueue(node.left);
+                    if (node.right is not null)
+                        queue.Enqueue(node.right);
+                }
+                list.Add(levelList);
+            }
+
+            list.ForEach(ProcessList);
+            return root;
+        }
+
+        private void ProcessList(List<Node> ll)
+        {
+            for (int i = 0; i < ll.Count - 1; i++)
+            {
+                ll.ElementAt(i).next = ll.ElementAt(i + 1);
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,21 +1,33 @@
 ﻿using LeetCode;
 using System.Collections;
 using System.Collections.Generic;
+using static LeetCode.MergeBinaryTrees;
+using static LeetCode.PopulateNextRightPointer;
 
 public class Solution
 {
     static void Main()
     {
-        int[][] a = new int[8][];
-        a[0] = new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        a[1] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 };
-        a[2] = new int[] { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
-        a[3] = new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 };
-        a[4] = new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 };
-        a[5] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
-        a[6] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 };
-        a[7] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 };
-        var sol = new MaxAreaOfIsland().maxAreaOfIsland(a);
+        //int[][] a = new int[8][];
+        //a[0] = new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        //a[1] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 };
+        //a[2] = new int[] { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+        //a[3] = new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 };
+        //a[4] = new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 };
+        //a[5] = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+        //a[6] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 };
+        //a[7] = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 };
+        //var sol = new MaxAreaOfIsland().maxAreaOfIsland(a);
+        //Console.WriteLine(sol);
+
+        var n1 = new Node(1);
+        n1.left = new Node(2);
+        n1.right = new Node(3);
+        n1.left.left = new Node(4);
+        n1.left.right = new Node(5);
+        n1.right.left = new Node(6);
+        n1.right.right = new Node(7);
+        var sol = new PopulateNextRightPointer().Connect(n1);
         Console.WriteLine(sol);
     }
 }
