4a380a120f56513f8ccfe9e6fb881676a2e2776c
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-01 01:41:48 +0300

    Nummber of 1 bits

---
+++ LeetCode/NumberOf1Bits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class NumberOf1Bits
+    {
+        /*Approach
+         * Consume and count 1s
+         */
+
+        //25 Mins
+        public int HammingWeight(uint n)
+        {
+            var count = 0;
+            while (n != 1 && n != 0) 
+            {
+                if(n % 2 == 1)
+                    count++;
+                n = n >> 1;
+            }
+            if(n == 1) count++;
+            return count;
+        }
+    }
+}
