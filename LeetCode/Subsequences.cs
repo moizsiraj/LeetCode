adccfb8f0deffa120c2b77bf63edf5ed5e4f26e5
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-04 02:47:34 +0300

    Sequences

---
+++ LeetCode/Subsequences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Subsequences
+    {
+        public void PrintAllSubsequences(int[] array) 
+        {
+            PrintSequence(array, new List<int>(), 0);
+        }
+
+        private void PrintSequence(int[] array, List<int> seq, int index) 
+        {
+            if (index >= array.Length)
+            {
+                seq.ForEach(Console.Write);
+                Console.WriteLine();
+                return;
+            }
+            seq.Add(array[index]);
+            PrintSequence(array, seq, index + 1);
+            seq.Remove(array[index]);
+            PrintSequence(array, seq, index + 1);
+        }
+    }
+}
