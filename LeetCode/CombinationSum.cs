497c82c2df0657f26c65845219255d6d692f7bcf
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-04 01:29:08 +0300

    Recursion Day 3

---
+++ LeetCode/CombinationSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class CombinationSumOne
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target, bool once)
+        {
+            var listOfList = new List<IList<int>>();
+            PopulateList(candidates, 0, listOfList, new List<int>(), 0, target);
+            return listOfList;
+        }
+
+        private void PopulateList(int[] candidates, int index, List<IList<int>> listOfList, List<int> list, int sum, int target)
+        {
+            if (index >= candidates.Length)
+            {
+                if (sum == target)
+                    listOfList.Add(new List<int>(list));
+                return;
+            }
+
+            list.Add(candidates[index]);
+            sum += candidates[index];
+            if (sum <= target)
+                PopulateList(candidates, index, listOfList, list, sum, target);
+            list.Remove(candidates[index]);
+            sum -= candidates[index];
+            PopulateList(candidates, index + 1, listOfList, list, sum, target);
+        }
+    }
+}
---
+++ LeetCode/CombineSumTwo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class CombineSumTwo
+    {
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            var listOfList = new List<IList<int>>();
+            Array.Sort(candidates);
+            PopulateListOfList(candidates, 0, 0, target, listOfList, new List<int>());
+            return listOfList;
+        }
+
+        private void PopulateListOfList(int[] candidates, int index, int sum, int target, List<IList<int>> listOfList, List<int> list)
+        {
+
+            if (sum == target)
+            {
+                listOfList.Add(new List<int>(list));
+                return;
+            }
+
+            for (int i = index; i < candidates.Length; i++)
+            {
+                if (i > index && candidates[i] == candidates[i - 1]) continue;
+
+                if (candidates[i] > target) break;
+
+                list.Add(candidates[i]);
+                sum += candidates[i];
+                PopulateListOfList(candidates, i + 1, sum, target, listOfList, list);
+                list.Remove(candidates[i]);
+                sum -= candidates[i];
+            }
+        }
+    }
+}
