eacd11fa7fe09098fb01efd2b5d017786262b88b
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-16 21:34:37 +0300

    Partitioning

---
+++ LeetCode/PalindromePartitioning.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class PalindromePartitioning
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            var listOfList = new List<IList<string>>();
+            PopulateListOfList(s, listOfList, new List<string>(), 0);
+            return listOfList;
+        }
+
+        private void PopulateListOfList(string s, List<IList<string>> listOfList, List<string> list, int index)
+        {
+            //Add list to result when partition get to the end of the array
+            if (index == s.Length)
+            {
+                listOfList.Add(new List<string>(list));
+                return;
+            }
+
+            //loop through all the indexes starting from index to n
+            for (int i = index; i < s.Length; i++)
+            {
+                //if index to i substring is a palindrome
+                if (StringIsPalindrome(s, index, i)) 
+                {
+                    var lenght = i + 1 - index;
+                    var substring = s.Substring(index, lenght);
+                    //add substring to the list
+                    list.Add(substring);
+                    //call function for the next index
+                    PopulateListOfList(s, listOfList, list, i + 1);
+                    //remove this added substring from the list to
+                    //maintain the length of the string, if we dont
+                    //remove this the lenght will become invalid
+                    //and partitioning too
+                    list.RemoveAt(list.Count - 1);
+                }
+            }
+        }
+
+        private bool StringIsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s.ElementAt(left) == s.ElementAt(right))
+                {
+                    left++;
+                    right--;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/PalindromePartitioningTwo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class PalindromePartitioningTwo
+    {
+        public int MinCut(string s)
+        {
+            return PopulateListOfList(s, new List<string>(), 0, int.MaxValue);
+        }
+
+        private int PopulateListOfList(string s, List<string> list, int index, int currentMin)
+        {
+            //when partition gets to end of the string check length with current minimum and return minimum
+            if (index == s.Length)
+            {
+                return Math.Min(currentMin, list.Count - 1);
+            }
+
+            //loop thtough index till n and check for substrings
+            for (int i = index; i < s.Length; i++)
+            {
+                //if substring from index till i is a palindrome 
+                if (StringIsPalindrome(s, index, i))
+                {
+                    var lenght = i + 1 - index;
+                    var substring = s.Substring(index, lenght);
+                    //Add substring to the list
+                    list.Add(substring);
+                    //make call for the next index
+                    currentMin = Math.Min(PopulateListOfList(s, list, i + 1, currentMin), currentMin);
+                    //remove this added substring from the list to
+                    //maintain the length of the string, if we dont
+                    //remove this the lenght will become invalid
+                    //and partitioning too
+                    list.Remove(substring);
+                }
+            }
+            return currentMin;
+        }
+
+        private bool StringIsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s.ElementAt(left) == s.ElementAt(right))
+                {
+                    left++;
+                    right--;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
