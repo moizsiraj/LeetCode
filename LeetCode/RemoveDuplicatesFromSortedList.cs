63f130002552f7989fee6db23ae1bf5825d31342
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-22 01:29:43 +0300

    Remove Duplicates from LL

---
+++ LeetCode/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class RemoveDuplicatesFromSortedList
+    {
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+        public LinkedList RemoveDuplicatesFromLinkedList(LinkedList linkedList)
+        {
+            if (linkedList.next == null)
+                return linkedList;
+
+            var current = linkedList;
+            var next = linkedList.next;
+
+            while (next != null)
+            {
+                if (current.value == next.value)
+                {
+                    current.next = next.next;
+                    next = current.next;
+                }
+                else
+                {
+                    current = current.next;
+                    next = next.next;
+
+                }
+            }
+
+            return linkedList;
+        }
+    }
+}
