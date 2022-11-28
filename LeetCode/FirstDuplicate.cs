5dd5a6f8c429a1a5a5bc45701c809308268a050a
Moiz Siraj <abdulmoiz68@gmail.com>
2022-11-28 22:40:24 +0300

    First duplicate value in array

---
+++ LeetCode/FirstDuplicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class FirstDuplicate
+    {
+        /*Approach
+         * keep adding to hashset
+         * return number that map already contains
+         * */
+        public int FirstDuplicateValue(int[] array)
+        {
+            var map = new HashSet<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (map.Contains(array[i]))
+                    return array[i];
+                map.Add(array[i]);
+            }
+            return -1;
+        }
+    }
+}
